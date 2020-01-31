a124e7f37c28cb01b088a73d020864ca9ff1665f
miyoungyoon <fitmiyoung@gmail.com>
2020-01-31 17:16:29 +0900

    EDA작업중: Age Group 도표 만들다 x축에 표시안되는 것까지

--- titanic/titanic_EDA.R
+++ titanic/titanic_EDA.R
@@ -6,6 +6,9 @@ library(tidyverse)
 data_t <- read.csv("/Users/miyoungyoon/kaggle/titanic/titanic_data/test.csv")
 data_tr <- read.csv("/Users/miyoungyoon/kaggle/titanic/titanic_data/train.csv")
 
+train <- read.csv("/Users/miyoungyoon/kaggle/titanic/titanic_data/train.csv")
+
+
 str(data_t)
 head(data_t)
 View(data_t)
@@ -78,17 +81,55 @@ length(data_tr$Cabin)
 colSums(is.na(data_tr))
 colSums(is.na(data_t))
 levels(data_tr$Cabin) #cabin이 factor임. 그래서 빈칸은 1개의 펙터로 취급하나? 
+# (Caution: Sometimes a missing value might be read into R as a blank or empty string.)https://rpubs.com/spring16/184493
+# 빈칸일 때 NA로 취급하게 만들어야 하나?
+is.null(data_tr$Cabin)
+is_empty(data_tr$Cabin[3])
+
+data_tr$Cabin[3]
+sum(data_tr$Cabin == "")
+sum(data_tr$Embarked == "")
+
+
+length(data_tr$Embarked)
+data_tr$Embarked
+is.na(data_tr$Embarked) 
+is.null(data_tr$Embarked)
 
 
 sapply(data_tr, function(x) sum(is.na(x)))
+# na로 못 찾는 것 "" 으로 찾음 
+sapply(data_tr, function(x) sum(x == ""))
+#is.na로 못 찾은 Cabin, Embarked 의 빈 칸 찾아냄. 
+# Cabin    Embarked   
+# 687           2 
 
+
+# Missing values: Embarked - "S"로 변경 
+class(data_tr$Embarked) #factor
+levels(data_tr$Embarked)
+embarked_empty<-data_tr$Embarked==""
+sum(is.na(embarked_empty))
+data_tr$Embarked[which(is.na(data_tr$Embarked))]<-"S" 
+data_tr$Embarked
+#factor 타입일 때, factor요소로 안 넣으면 에러 남. 
+ 
+
+# Missing values: Cabin - "번호 없으면 0, 있으면 1로 변환"
+class(data_tr$Cabin)
+data_tr$Cabin <- lapply(data_tr$Cabin, as.character)
+data_tr$Cabin <- ifelse(data_tr$Cabin=="", 0, 1)
+data_tr$Cabin
+
+
+# Age는 평균값으로 채워넣기 
 missing_values %>% 
   ggplot(aes(x=reorder(feature,-missing_pct),y=missing_pct)) +
   geom_bar(stat="identity",fill="red")+
   coord_flip()+theme_bw()
 
-# ?? . Missing Value: Age, Survived, Cabin 어떻게 처리해야 하나?
-# Age는 평균값으로 채워넣기 
+# Data manupulation
+# 나이 분류해 그룹화 
 data_tr<-data_tr %>%
   mutate(
     Age = ifelse(is.na(Age), mean(data_tr$Age, na.rm=TRUE), Age),
@@ -97,6 +138,146 @@ data_tr<-data_tr %>%
                             Age >= 18 & Age <60 ~ "Age.1859",
                             Age >= 60 ~ "Age.60Ov"))
 
-View(data_tr)
+#이름의 타이틀 
+names <- data_tr$Name
+title <-  gsub("^.*, (.*?)\\..*$", "\\1", names)
+data_tr$title <- title
+table(title)
+
+data_tr$title[data_tr$title %in% c("Mlle","Ms","Lady","Dona")] <-"Miss"
+data_tr$title[data_tr$title=="Mme"]<-"Mrs"
+data_tr$title[!(data_tr$title %in% c("Miss", "Mr", "Mrs"))] <-"Rare title"
+data_tr$title <-as.factor(data_tr$title)
+table(data_tr$Sex, data_tr$title)
+
+data_tr %>%
+  filter(Sex=="female", title=="Rare title")
+
+data_tr$Survived<-factor(data_tr$Survived)
+ggplot(data_tr, aes(title, fill=factor(Survived))) +
+  geom_bar(stat="count", position="stack")+
+  labs(x="title") + theme_gray()
+#데이터가 factor 타입이 아니면 fill 이 안 먹으므로 factor()함수로 카테고리형 데이터로 변환 시켜야 함.
+
+#Family Groups
+#형제/배우자, 부모/자식 
+data_tr$Fsize <- data_tr$SibSp+data_tr$Parch + 1 #1을 추가하는 것은 형제, 부모 없을 때 signle 로 표현하기 위해서
+
+# 도표를 보면서 가족 수 그룹핑할 것 파악 
+ggplot(data_tr, aes(x=Fsize, fill=Survived))+
+  geom_bar(stat='count', position='dodge') 
+
+sum(Fsize) #integer 값의 합. 행의 개수가 아님. 
+count(Fsize) #integer는 카운트 함수가 안 먹음. 
+class(Fsize)
+
+Fsize_df<-as.data.frame(Fsize)
+count(Fsize_df)
+dim(Fsize_df)
+
+data_tr$FamilySized[data_tr$Fsize == 1] <- 'Single'
+data_tr$FamilySized[data_tr$Fsize < 5 & Fsize >= 2] <-'Small'
+data_tr$FamilySized[data_tr$Fsize >= 5 ]<-'Big'
+data_tr$FamilySized = as.factor(data_tr$FamilySized)
+
+
+#고유번호를 골라서 재번호 부여하는 방법은? 
+# Tickets: 번호의 중복 개수 추출 
+ticket.unique <- rep(0, nrow(data_tr))
+tickets <- unique(data_tr$Ticket)
+tickets
+length(tickets) #681개의 고유 티켓 번호 
+for(i in 1:length(tickets)){ #4번째 티켓 들어오면, 
+  current.ticket <- tickets[i] #current.ticket = ticket 4번째 고유 번호 '113803'들어감 
+  party.indexes <- which(data_tr$Ticket == current.ticket) #현재 보고 있는 티켓번호와 고유번호가 매칭되는 행 번호가 저장됨 
+  
+  for(k in 1:length(party.indexes)){ 
+    ticket.unique[party.indexes[k]] <- length(party.indexes)
+  }
+}
+
+data_tr$ticket.unique <- ticket.unique
+
+data_tr %>%
+  filter(Ticket == "349909")
+
+
+data_tr$ticket.size[data_tr$ticket.unique == 1]<-'Single'
+data_tr$ticket.size[data_tr$ticket.unique <5 & data_tr$ticket.unique >=2] <- 'Small'
+data_tr$ticket.size[data_tr$ticket.unique >= 5] <-'Big'
+
+ggplot(data_tr, aes(x=ticket.unique, fill=Survived)) +
+  geom_bar(stat='count', position='dodge')+
+  scale_x_continuous(breaks=c(1:11))+
+  labs(x='Ticket Size') + theme_grey()
+
+
+data_tr<-data_tr %>%
+  mutate(Survived = case_when(Survived == 1 ~ 'Yes',
+                              Survived == 0 ~ 'No'))
+
+crude_summary <- data_tr %>%
+  select(PassengerId, Survived) %>%
+  group_by(Survived) %>%
+  summarise(n=n()) %>%
+  mutate(freq = n/sum(n))
+
+crude_survrate <- crude_summary$freq[crude_summary$Survived=="Yes"]
+
+#EDA
+#Pclass
+ggplot(data_tr, aes(Pclass, fill=Survived)) +
+  geom_bar(position = "fill") +
+  ylab("Survival Rate") +
+  geom_hline(yintercept=crude_survrate, col="white", lty=2, size=2)+
+  ggtitle("Survival Rate by Class") +
+  theme_minimal()+
+  scale_y_continuous(labels = scales::percent) 
+
+
+#Sex
+ggplot(data_tr, aes(Sex, fill=Survived)) + 
+  geom_bar(position="fill")+
+  ylab("Survival Rate") +
+  geom_hline(yintercept=crude_survrate, col="white", lty=2, size=2)+
+  ggtitle("Survival Rate by Sex") +
+  theme_minimal()+
+  scale_y_continuous(labels=scales::percent)
+
+#Age: 제대로 표현 안됨 
+ggplot(data_tr, aes(Age, fill=Survived)) +
+  geom_bar(position="fill")+
+  ylab("Survival Rate")+
+  scale_y_continuous(labels=scales::percent)+
+  geom_hline(yintercept=crude_survrate, col="white", lyt=2, size=2)+
+  ggtitle("Survival Rate by Age") + 
+  theme_minimal()
+
+# Age_v2
+tbl_age <- data_tr %>%
+  select(Age, Survived) %>%
+  group_by(Survived) %>%
+  summarise(mean.age = mean(Age, na.rm=TRUE))
+
+tbl_age
+
+ggplot(data_tr, aes(Age, fill=Survived)) + 
+  geom_histogram(aes(y=..density..), alpha=0.5)+
+  geom_density(alpha=.2, aes(colour=Survived))+
+  geom_vline(data=tbl_age, aes(xintercept=mean.age, colour=Survived), lty=2, size=1)+
+  scale_y_continuous(labels=scales::percent)+
+  ylab("Density")+
+  ggtitle("Survival Rate by Age")+
+  theme_minimal()
+
+names(data_tr)
+
+data_tr$`Age Group`<-as.factor(data_tr$`Age Group`)
+ggplot(data_tr, aes(x='Age Group', fill=Survived)) +
+  geom_bar(position="stack") 
+  
+levels(data_tr$`Age Group`)
+#나이 그룹이 4가지인데, x축에 안 나오고 있음. 이유를 모르겠음. 
+
 
 
